6f8cef8ddd37d94ef451af59d1ccf4a0b7113619
DannyLien <danny.lien1109@gmail.com>
2024-09-19 16:41:34 +0800

    Initial commit , Kotlin Class Hello.kt var val 變數

---
+++ app/src/main/java/com/hank/bmi/Hello.kt
@@ -0,0 +1,20 @@
+package com.hank.bmi
+
+class Hello {
+
+
+}
+
+fun main() {
+    println("Hello Kotlin!")
+    val age = 20
+    println(age.inc())
+    println(age)
+
+    val name ="Hank"
+    println(name.get(1))
+//    name = "Jack"
+
+
+}
+
