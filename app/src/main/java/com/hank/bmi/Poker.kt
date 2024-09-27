a7f3c00cccbac69a8f6e7aaa695453cdd07ead34
DannyLien <danny.lien1109@gmail.com>
2024-09-27 16:56:44 +0800

    2-16, Poker ex, if else is expression, 變數後接 if else 判斷式;

---
+++ app/src/main/java/com/hank/bmi/Poker.kt
@@ -0,0 +1,34 @@
+package com.hank.bmi
+
+fun main() {
+    val deck = mutableListOf<Int>()
+    for (i in (0 until 52)) {
+        deck.add(i)
+        if (i % 13 == 0 && i != 0) println()
+        val a = i / 13
+        var type = if (a == 0) {
+            "C"
+        } else if (a == 1) {
+            "D"
+        } else if (a == 2) {
+            "H"
+        }else "S"
+
+        print("${(i % 13) + 1}${type} ")
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
