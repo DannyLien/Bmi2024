47bba977aeaf4756126f7213d3b954ae315a5581
DannyLien <danny.lien1109@gmail.com>
2024-09-27 16:17:28 +0800

    2-12, For迴圈, arrayof陣列

--- app/src/main/java/com/hank/bmi/Variable.kt
+++ app/src/main/java/com/hank/bmi/Variable.kt
@@ -1,25 +1,51 @@
 package com.hank.bmi
 
-fun main() {
-    var name: String? = "Hank"
-    println(name?.length)
-    println(name ?: 0)
+import androidx.compose.runtime.sourceInformation
 
-    var counter = 0
-    counter = counter + 1
-    counter++
-    counter = counter - 1
-    println(counter)
-    println(10 / 3.0)
-    //Boolean
-    var b = false
-    var a = false
-    println(a && b)
-    println(a || b)
-    //
-    val num = -8
-    println(num <= 5 && num >= -3)
-    //
-    println(num > 5 || num < -3)
+fun main() {
+    //List
+    val array = intArrayOf(2, 5, 8) //0,1,2
+    println(array[2])   //[3]exception
+    val ss = arrayOf("SUM", "MON", "TUE")
+    println(ss.size)
+    println(ss.get(0))
+    for (day in ss) {
+        println(day)
+    }
+    //For
+    val range = 0..10 //0~10
+    for (i in 1..30) {
+        if (i % 2 == 1) {
+            print("*")
+        } else {
+            print(" ")
+        }
+    }
+    println()
+    for (i in 1..10 step 2) {
+        print(i)
+    }
+    println()
+//    //Null
+//    var name: String? = "Hank"
+//    println(name?.length)
+//    println(name ?: 0)
+//
+//    var counter = 0
+//    counter = counter + 1
+//    counter++
+//    counter = counter - 1
+//    println(counter)
+//    println(10 / 3.0)
+//    //Boolean
+//    var b = false
+//    var a = false
+//    println(a && b)
+//    println(a || b)
+//    //
+//    val num = -8
+//    println(num <= 5 && num >= -3)
+//    //
+//    println(num > 5 || num < -3)
 
 }
