f68183343f679888130af7f2d5d88959fe8f2e3f
DannyLien <danny.lien1109@gmail.com>
2024-09-27 16:42:22 +0800

    2-14, mutableof removeAt shuffe;

--- app/src/main/java/com/hank/bmi/Variable.kt
+++ app/src/main/java/com/hank/bmi/Variable.kt
@@ -3,29 +3,47 @@ package com.hank.bmi
 import androidx.compose.runtime.sourceInformation
 
 fun main() {
+    //
+    val nums = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+//    val secret = nums.shuffle()
+    nums.shuffle()
+    val secret = nums.removeAt(0)
+    val bomb = nums.removeAt(0)
+    println(nums)
+    println(secret)
+    println(bomb)
+
     //List
-    val array = intArrayOf(2, 5, 8) //0,1,2
-    println(array[2])   //[3]exception
-    val ss = arrayOf("SUM", "MON", "TUE")
-    println(ss.size)
-    println(ss.get(0))
-    for (day in ss) {
-        println(day)
-    }
-    //For
-    val range = 0..10 //0~10
-    for (i in 1..30) {
-        if (i % 2 == 1) {
-            print("*")
-        } else {
-            print(" ")
-        }
-    }
-    println()
-    for (i in 1..10 step 2) {
-        print(i)
-    }
-    println()
+    val list = mutableListOf<Int>(2, 5, 8)
+    println(list)
+    list.add(13)
+    println(list)
+    list.removeAt(1)
+    println(list)
+
+    //Array
+//    val array = intArrayOf(2, 5, 8) //0,1,2
+//    println(array[2])   //[3]exception
+//    val ss = arrayOf("SUM", "MON", "TUE")
+//    println(ss.size)
+//    println(ss.get(0))
+//    for (day in ss) {
+//        println(day)
+//    }
+//    //For
+//    val range = 0..10 //0~10
+//    for (i in 1..30) {
+//        if (i % 2 == 1) {
+//            print("*")
+//        } else {
+//            print(" ")
+//        }
+//    }
+//    println()
+//    for (i in 1..10 step 2) {
+//        print(i)
+//    }
+//    println()
 //    //Null
 //    var name: String? = "Hank"
 //    println(name?.length)
