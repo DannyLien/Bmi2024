9e8e909663715ba505938fce2b2edf6334819fe7
DannyLien <danny.lien1109@gmail.com>
2024-09-27 15:14:58 +0800

    2-11, while() 迴圈條件停止

--- app/src/main/java/com/hank/bmi/Guessing.kt
+++ app/src/main/java/com/hank/bmi/Guessing.kt
@@ -1,19 +1,24 @@
 package com.hank.bmi
 
 fun main() {
-    val scret = 7
-    print("Please enter a number(1-10): ")
-    val input = readLine()
-    val num: Int = input?.toIntOrNull() ?: 0
-    println("The number you entered: ${num} ")
+    val secret = 7
+    var num = 0
+    while (num != secret) {
+        print("Please enter a number(1-10): ")
+        val input = readLine()
+        num = input?.toIntOrNull() ?: 0
+        println("The number you entered: ${num} ")
 
-    if (num < scret) {
-        println("Bigger")
-    } else if (num > scret) {
-        println("Smaller")
-    } else {
-        println("You got it!")
+        if (num < secret) {
+            println("Bigger")
+        } else if (num > secret) {
+            println("Smaller")
+        } else {
+            println("You got it!")
+
+        }
     }
+
 
 }
 
