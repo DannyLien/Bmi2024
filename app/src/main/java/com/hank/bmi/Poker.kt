aed2cfade0004ef3ec4b2c7f155a8f1a7e39eb17
DannyLien <danny.lien1109@gmail.com>
2024-09-30 09:24:36 +0800

    3-2, List Set shuffled ;

--- app/src/main/java/com/hank/bmi/Poker.kt
+++ app/src/main/java/com/hank/bmi/Poker.kt
@@ -1,6 +1,17 @@
 package com.hank.bmi
 
 fun main() {
+    //Set
+    val set = setOf<Int>(5, 3, 9, 3, 2, 1, 9)
+    println(set)
+    set.forEach { num ->
+        println(num)
+    }
+
+    //C, D, H, S
+    val list = listOf<Int>(5, 3, 8, 5, 3, 2, 1)
+    println(list)
+
     val deck = mutableListOf<Int>()
     for (i in (0 until 52)) {
         deck.add(i)
@@ -23,6 +34,17 @@ fun main() {
 
         print("${(i % 13) + 1}${type} ")
     }
+//    println()
+//    println(deck)
+//
+//    val deck2 = deck.shuffled()
+//    println(deck)
+//    println(deck2)
+//
+//    val a = deck.shuffled().first()
+//    val b = deck.shuffled().first()
+//    println(a)
+//    println(b)
 
 }
 
