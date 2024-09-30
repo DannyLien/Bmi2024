5185da6623d5953e3a425a045ae73de32fdf7d08
DannyLien <danny.lien1109@gmail.com>
2024-09-30 23:52:56 +0800

    3-6, 優化類別建構子設計, 回傳數值, 使用 list + For loop;

--- app/src/main/java/com/hank/bmi/student/Student.kt
+++ app/src/main/java/com/hank/bmi/student/Student.kt
@@ -2,24 +2,47 @@ package com.hank.bmi.student
 
 class Student(
     val id: String, val name: String,
-    var english: Int, val math: Int
+    var english: Int = 0, var math: Int = 0
 ) {
-    constructor(id: String, name: String) : this(id, name, 0, 0)
+//    constructor(id: String, name: String) : this(id, name, 0, 0)
 
     fun print() {
-        println("$id\t$name\t$english\t$math")
+        println("$id\t$name\t$english\t$math\t${average()}\t${grading()}")
+    }
+
+    fun average() = (english + math) / 2
+
+    fun grading() = when (average() / 10) {
+        9, 10 -> "A"
+        8 -> "B"
+        7 -> "C"
+        6 -> "D"
+        else -> "F"
     }
 
 }
 
 
 fun main() {
-    val stu = Student("001", "Jack", 70, 80)
-    val stu2 = Student("002", "Hank", 66, 87)
-    val stu3 = Student("003", "Jane")
-    stu.print()
-    stu2.print()
-    stu3.print()
+    val students = listOf<Student>(
+        Student("001", "Jack", 95, 99),
+        Student("002", "Hank", 66, 87),
+        Student("003", "Jane")
+    )
+    for (i in (0..2)) {
+        students.get(i).print()
+    }
+    for (i in students) {
+        i.print()
+    }
+    students.forEach {
+        it.print()
+    }
+
+
+//    stu.print()
+//    stu2.print()
+//    stu3.print()
 
 }
 
