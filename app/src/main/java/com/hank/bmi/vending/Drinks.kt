d70309c9f3af713dd5d6eac6e3322c6510d2aeea
DannyLien <danny.lien1109@gmail.com>
2024-10-03 21:52:16 +0800

    4-3, kotlin data class

--- app/src/main/java/com/hank/bmi/vending/Drinks.kt
+++ app/src/main/java/com/hank/bmi/vending/Drinks.kt
@@ -1,6 +1,11 @@
-package com.hank.bmi
+package com.hank.bmi.vending
 
 fun main() {
+    val tea = Drink("Black tea",0,50)
+    println(tea)
+    val tea2 = tea.copy(suger=5)
+    println(tea2)
+
     val drinks = mutableMapOf<Int, String>(
         1 to "Coke",
         2 to "7-UP",
