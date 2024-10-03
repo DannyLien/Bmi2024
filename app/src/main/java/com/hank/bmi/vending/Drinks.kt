445cd20bf4b6ba24a6dd4f6eb5c24b633c9d0bd5
DannyLien <danny.lien1109@gmail.com>
2024-10-03 22:00:28 +0800

    4-4, kotlin extension function 擴充功能;

--- app/src/main/java/com/hank/bmi/vending/Drinks.kt
+++ app/src/main/java/com/hank/bmi/vending/Drinks.kt
@@ -1,9 +1,18 @@
 package com.hank.bmi.vending
 
+
+fun Drink.off20(): Drink {
+    return this.copy(price = (this.price * 0.8).toInt())
+}
+
 fun main() {
-    val tea = Drink("Black tea",0,50)
+    val s = "abcef"
+    println(s.validate())
+    val tea = Drink("Black tea", 0, 50)
+    val discountTea = tea.off20()
     println(tea)
-    val tea2 = tea.copy(suger=5)
+    println(discountTea)
+    val tea2 = tea.copy(suger = 5)
     println(tea2)
 
     val drinks = mutableMapOf<Int, String>(
@@ -17,6 +26,10 @@ fun main() {
     drinks.set(5, "Water")
     println(drinks)
 
+}
+
+fun String.validate(): Boolean {
+    return this.length >= 6
 }
 
 
