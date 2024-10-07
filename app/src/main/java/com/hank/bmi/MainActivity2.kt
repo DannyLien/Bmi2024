97db4a199491a46b0917d5e419bce505c60cd79c
DannyLien <danny.lien1109@gmail.com>
2024-10-07 22:50:59 +0800

    5-6, 存取畫面元件 , 輸入方塊 , 按鈕元件 , 互動程式設計 ;

--- app/src/main/java/com/hank/bmi/MainActivity2.kt
+++ app/src/main/java/com/hank/bmi/MainActivity2.kt
@@ -2,9 +2,12 @@ package com.hank.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.hank.bmi.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private val TAG: String? = MainActivity2::class.java.simpleName
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,15 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(binding.root)
 
 
+    }
+
+    fun guess(view: View) {
+        if (!binding.number.text.toString().equals("")) {
+            val num = binding.number.text.toString().toInt()
+            Log.d(TAG, "guess: $num")
+
+        }
 
     }
+
 }
