6d8cf6137a7b3e952a3d561e3f942493018102d8
DannyLien <danny.lien1109@gmail.com>
2024-10-09 23:26:30 +0800

    6-2, Composable

--- app/src/main/java/com/hank/bmi/MainActivity.kt
+++ app/src/main/java/com/hank/bmi/MainActivity.kt
@@ -3,13 +3,22 @@ package com.hank.bmi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.material.search.SearchBar
 import com.hank.bmi.ui.theme.BmiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Android555")
                 }
             }
         }
@@ -31,16 +40,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = "Hello Full $name!",
+            modifier = modifier
+        )
+        Text(text = "Text2")
+        Text(text = "Text3 blablabla")
+    }
+
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    Column {
+        TextField(
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "Search")
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = ""
+                )
+            },
+            modifier = modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BmiTheme {
-        Greeting("Android")
+//        Greeting("Android222")
+        SearchBar()
     }
 }
