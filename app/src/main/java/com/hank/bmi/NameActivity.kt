b738b37aa8fd1d4c176608b82eb80927ae853122
DannyLien <danny.lien1109@gmail.com>
2024-11-07 15:57:31 +0800

    11-3, Spinner ;

--- app/src/main/java/com/hank/bmi/NameActivity.kt
+++ app/src/main/java/com/hank/bmi/NameActivity.kt
@@ -3,6 +3,7 @@ package com.hank.bmi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,10 +27,9 @@ class NameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //Json Gson MVVM
         myViewModel = ViewModelProvider(this).get(MyViewModel::class.java)
         myViewModel.readJSON()
-
         var recycler = binding.recycler
         recycler.setHasFixedSize(true)
         recycler.layoutManager = LinearLayoutManager(this)
@@ -37,6 +37,14 @@ class NameActivity : AppCompatActivity() {
         myViewModel.words.observe(this) {
             recycler.adapter = WordAdapter(it)
         }
+        //Spinner
+        val nameAdapter = ArrayAdapter<String>(
+            this, android.R.layout.simple_spinner_item, names
+        )
+        nameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinner = binding.spinner
+        spinner.adapter = nameAdapter
+        spinner.prompt = "Seclect Name"
 
     }
 
